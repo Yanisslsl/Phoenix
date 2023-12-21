fbd230047c084e6069c6cff5e39c4ebd2bf60f37
Yanisslsl <loisel.yaniss@ynov.com>
2023-12-21 23:11:48 +0100

    feat EVENT SYSTEM: add Event Dispatcher and Events"

--- Phoenix/Core/Events/include/Event.h
+++ Phoenix/Core/Events/include/Event.h
@@ -38,33 +38,6 @@ namespace Phoenix
         virtual std::string ToString() const { return GetName(); }
     };
 
-    // NOTE: Maybe pass the system to an queue based dispatcher ?
-    class EventDispatcher
-    {
-    public:
-        EventDispatcher(Event& event)
-            : m_event(event)
-        {
-        }
-
-        template<typename T, typename F>
-        bool Dispatch(const F& func)
-        {
-            if (m_event.GetEventType() == T::GetStaticType())
-            {
-                m_event.Handled |= func(static_cast<T&>(m_event));
-                return true;
-            }
-            return false;
-        }
-
-        inline std::ostream& operator<<(std::ostream& os, const Event& e)
-        {
-            return os << e.ToString();
-        }
-
-    private :
-        Event& m_event;
-    };
+   
 }
 
--- Phoenix/Core/Events/include/EventDispatcher.h
+++ Phoenix/Core/Events/include/EventDispatcher.h
@@ -1,7 +1,36 @@
 ﻿#pragma once
+#include "Event.h"
+#include <functional>
 
-class EventDispatcher
+
+namespace Phoenix
 {
-public:
-    
-};
+    // NOTE: Maybe pass the system to an queue based dispatcher ?
+    class EventDispatcher
+    {
+    public:
+        EventDispatcher(Event& event)
+            : m_event(event)
+        {
+        }
+
+        template<typename T, typename F>
+        bool Dispatch(const F& func)
+        {
+            if (m_event.GetEventType() == T::GetStaticType())
+            {
+                m_event.Handled |= func(static_cast<T&>(m_event));
+                return true;
+            }
+            return false;
+        }
+
+        inline std::ostream& operator<<(std::ostream& os, const Event& e)
+        {
+            return os << e.ToString();
+        }
+
+        private :
+            Event& m_event;
+    };
+}
